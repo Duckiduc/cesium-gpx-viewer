d3da39209ca8f2962edc1be0fc19937fe2ae53b7
Duc-Thomas <duckiduc.dev@gmail.com>
2023-07-15 03:17:02 +0200

    Add GpxForm

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -10,75 +10,49 @@ import {
 import { useEffect, useRef, useState } from 'react'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 import './App.css'
-import GPX from './assets/moto.gpx'
 import ApiForm from './components/ApiForm'
+import GpxForm from './components/GpxForm'
 
 function App(): JSX.Element {
   const [apiKey, setApiKey] = useState('')
+  const [GPX, setGPX] = useState<File | null>(null)
   const viewerRef = useRef<Viewer | null>(null)
 
   useEffect(() => {
     const initializeViewer = async (): Promise<void> => {
-      const CESIUM_ACCESS_TOKEN = apiKey
-
-      Ion.defaultAccessToken = CESIUM_ACCESS_TOKEN
+      Ion.defaultAccessToken = apiKey
 
       // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID
       viewerRef.current = new Viewer('cesiumContainer', {
         terrain: Terrain.fromWorldTerrain({
-          requestVertexNormals: true
+          requestVertexNormals: false
         })
       })
 
       viewerRef.current.scene.globe.enableLighting = true
 
-      viewerRef.current.dataSources.add(GpxDataSource.load(GPX))
-
       console.log(Color.YELLOW)
 
-      // viewerRef.current.dataSources.add(
-      //   GpxDataSource.load('/src/assets/raye.gpx', {
-      //     clampToGround: true,
-      //     trackColor: Color.YELLOW.toCssColorString()
-      //   })
-      // )
+      if (viewerRef.current && GPX) {
+        const dataSource = new GpxDataSource()
+        dataSource.load(GPX, {
+          clampToGround: true
+          // trackColor: Color.YELLOW.toCssColorString()
+        })
+        viewerRef.current.dataSources.add(dataSource)
+      }
+    }
 
-      // Fly the camera to San Francisco at the given longitude, latitude, and height
-      // viewerRef.current.camera.flyTo({
-      //   destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-      //   orientation: {
-      //     heading: CesiumMath.toRadians(0.0),
-      //     pitch: CesiumMath.toRadians(-15.0),
-      //   }
-      // })
-
-      // Add Cesium OSM Buildings, a global 3D buildings layer
-      // const buildingTileset = await createOsmBuildingsAsync()
-      // viewerRef.current.scene.primitives.add(buildingTileset)
+    const destroyViewer = (): void => {
+      viewerRef.current?.destroy()
     }
 
     if (apiKey) {
       initializeViewer()
     }
 
-    return () => {
-      viewerRef.current?.destroy()
-    }
-  }, [apiKey])
-
-  // const handleApiKeyChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-  //   setApiKey(event.target.value)
-  // }
-
-  // const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
-  //   event.preventDefault()
-  //   const form = event.currentTarget
-  //   const apiKeyInput = form.elements.namedItem('apiKey') as HTMLInputElement
-  //   const enteredApiKey = apiKeyInput.value
-  //   // Validate the API key if needed
-  //   // You can add your validation logic here before setting the API key
-  //   setApiKey(enteredApiKey)
-  // }
+    return destroyViewer
+  }, [apiKey, GPX])
 
   const handleFormSubmit = (value: string): void => {
     // Do something with the input value in the parent component
@@ -86,16 +60,26 @@ function App(): JSX.Element {
     setApiKey(value)
   }
 
-  // // Add Cesium OSM Buildings, a global 3D buildings layer
-  // const buildingTileset = await createOsmBuildingsAsync()
-  // viewer.scene.primitives.add(buildingTileset)
-
   return (
     <>
       <div id="cesiumContainer"></div>
+      <GpxForm onFileUpload={(file: File): void => setGPX(file)} />
       {!apiKey && <ApiForm onSubmit={handleFormSubmit} />}
     </>
   )
 }
 
 export default App
+
+// Fly the camera to San Francisco at the given longitude, latitude, and height
+// viewerRef.current.camera.flyTo({
+//   destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+//   orientation: {
+//     heading: CesiumMath.toRadians(0.0),
+//     pitch: CesiumMath.toRadians(-15.0),
+//   }
+// })
+
+// Add Cesium OSM Buildings, a global 3D buildings layer
+// const buildingTileset = await createOsmBuildingsAsync()
+// viewerRef.current.scene.primitives.add(buildingTileset)
---
+++ src/renderer/src/components/GpxForm.tsx
@@ -0,0 +1,31 @@
+import './GpxForm.css'
+
+interface GpxFormProps {
+  onFileUpload: (file: File) => void
+}
+
+function GpxForm({ onFileUpload }: GpxFormProps): JSX.Element {
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const file = event.target.files?.[0]
+    if (file) {
+      onFileUpload(file)
+    }
+  }
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    const file = event.target[0].files?.[0]
+    if (file) {
+      onFileUpload(file)
+    }
+  }
+
+  return (
+    <form className="gpx-form" onSubmit={handleFormSubmit}>
+      <input type="file" accept=".gpx" onChange={handleFileChange} />
+      <button type="submit">Upload GPX</button>
+    </form>
+  )
+}
+
+export default GpxForm
