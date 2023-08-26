708557df9f857de80d2e70272adbc028048a2238
Duckiduc <git@ducki.dev>
2023-08-26 20:35:31 +0200

    Style form

--- src/renderer/src/components/GpxForm.tsx
+++ src/renderer/src/components/GpxForm.tsx
@@ -32,19 +32,33 @@ export function GpxForm({ onFileUpload }: GpxFormProps): JSX.Element {
 
   return (
     <div className="gpx-form">
-      <input type="file" accept=".gpx" multiple onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload GPX</button>
+      <div className='upload-container'>
+        <label htmlFor="upload">Choose files</label>
+        <input id='upload' type="file" accept=".gpx" multiple hidden onChange={handleFileChange} />
+        <button className='upload-btn' onClick={handleUpload}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path fill='#fff' d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+        </button>
+      </div>
+      <div className='gpx-container'>
       {selectedFiles.map((track, index) => (
-        <div key={index}>
+        <div key={index} className='file'>
           <span>{track.file.name}</span>
-          <input
-            type="color"
-            value={track.color}
-            onChange={(e): void => handleColorChange(index, e.target.value)}
-          />
-          <button onClick={(): void => handleDelete(index)}>Delete</button>
+          <div className='right'>
+            <div className='color-container'>
+              <input
+                type="color"
+                value={track.color}
+                onChange={(e): void => handleColorChange(index, e.target.value)}
+              />
+            </div>
+            <button onClick={(): void => handleDelete(index)} className='delete-btn'>
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path fill='#fff' d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>
+            </button>
+          
+          </div>
         </div>
       ))}
+      </div>
     </div>
   )
 }
