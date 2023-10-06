5f83550f163b96b99678703aa0b6b4513ca5298f
Duckiduc <git@ducki.dev>
2023-10-06 18:27:58 +0200

    Add JsonCard and style form

--- src/renderer/src/components/DayCard.tsx
+++ src/renderer/src/components/DayCard.tsx
@@ -11,13 +11,16 @@ export function DayCard({ weatherData, currentClock, mid }: DayCardProps): JSX.E
   console.log('🚀 ~ file: NowCard.tsx:8 ~ NowCard ~ currentClock:', currentClock)
   console.log('🚀 ~ file: NowCard.tsx:10 ~ NowCard ~ weatherData', weatherData)
   console.log('🚀 ~ file: NowCard.tsx:10 ~ NowCard ~ mid')
-  const hours = weatherData.days[0].hours
+
+  const hours = mid === "am" ? weatherData.days[0].hours.slice(0, 12) : weatherData.days[0].hours.slice(12);
+  console.log('🚀 ~ file: NowCard.tsx:10 ~ NowCard ~ hours', hours)
 
   return (
     <div className="card">
       {hours.map((hour, index) => (
         <div key={index} className="hour-cell">
-          <div>{index}</div>
+          <div>{mid === "am" ? index : index + 12}</div>
+          <br />
           <div>{hour.temp}</div>
         </div>
       ))}
