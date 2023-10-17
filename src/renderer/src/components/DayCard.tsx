a2bf4eeee68d7399bfe09bd12e142d02439d9176
Duckiduc <git@ducki.dev>
2023-10-17 22:27:15 +0200

    Handle am pm for DayCard

--- src/renderer/src/components/DayCard.tsx
+++ src/renderer/src/components/DayCard.tsx
@@ -1,25 +1,33 @@
 import { GregorianDate } from 'cesium'
 import './DayCard.css'
+import { WeatherData } from '../types/weatherData'
 
 interface DayCardProps {
-  weatherData: any
+  weatherData: WeatherData
   currentClock: GregorianDate | null
   mid: string
 }
 
 export function DayCard({ weatherData, currentClock, mid }: DayCardProps): JSX.Element {
-  console.log('🚀 ~ file: NowCard.tsx:8 ~ NowCard ~ currentClock:', currentClock)
-  console.log('🚀 ~ file: NowCard.tsx:10 ~ NowCard ~ weatherData', weatherData)
-  console.log('🚀 ~ file: NowCard.tsx:10 ~ NowCard ~ mid')
+  const hours =
+    mid === 'am' ? weatherData.days[0].hours.slice(0, 12) : weatherData.days[0].hours.slice(12)
 
-  const hours = mid === "am" ? weatherData.days[0].hours.slice(0, 12) : weatherData.days[0].hours.slice(12);
-  console.log('🚀 ~ file: NowCard.tsx:10 ~ NowCard ~ hours', hours)
+  const shift = weatherData.tzoffset > 0 ? `+${weatherData.tzoffset}` : weatherData.tzoffset
+  const currentClockTz = (currentClock?.hour ?? 0) + weatherData.tzoffset ?? 0
 
   return (
     <div className="card">
+      <div className="tz">UTC{shift}</div>
       {hours.map((hour, index) => (
-        <div key={index} className="hour-cell">
-          <div>{mid === "am" ? index : index + 12}</div>
+        <div
+          key={index}
+          className={`hour-cell ${
+            currentClockTz >= 0 && (mid === 'am' ? index : index + 12) === currentClockTz
+              ? 'black'
+              : ''
+          }`}
+        >
+          <div>{mid === 'am' ? index : index + 12}</div>
           <br />
           <div>{hour.temp}</div>
         </div>
