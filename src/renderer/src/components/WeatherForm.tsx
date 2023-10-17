bef459874a3ed86280e0b49411cab5461953bad9
Duckiduc <git@ducki.dev>
2023-10-17 22:24:18 +0200

    Move logic from weatherForm

--- src/renderer/src/components/WeatherForm.tsx
+++ src/renderer/src/components/WeatherForm.tsx
@@ -1,73 +1,22 @@
-import { Cartographic, GregorianDate, JulianDate, Math, Viewer } from 'cesium'
-import { RefObject, useState } from 'react'
+import { GregorianDate } from 'cesium'
 import './WeatherForm.css'
 import { NowCard } from './NowCard'
-import data from '../assets/weather-mock.json'
 import { DayCard } from './DayCard'
 import { JsonCard } from './JsonCard'
+import { WeatherData } from '../types/weatherData'
 
 interface WeatherFormProps {
-  viewerRef: RefObject<Viewer | null>
+  weatherData: WeatherData | null
+  currentClock: GregorianDate | null
 }
 
-export function WeatherForm({ viewerRef }: WeatherFormProps): JSX.Element {
-  const [currentClock, setCurrentClock] = useState<GregorianDate | null>(null)
-
-  const getCoordinates = (): { latitude: number; longitude: number } => {
-    if (viewerRef.current) {
-      const mainDataSource = viewerRef.current.dataSources.get(0)
-      const activeEntity = (mainDataSource.entities as any)._entities._array[0]
-      const activeEntityPosition = activeEntity._position.getValue(
-        viewerRef.current.clock.currentTime
-      )
-
-      const date = JulianDate.toGregorianDate(viewerRef.current.clock.currentTime)
-
-      setCurrentClock(date)
-
-      console.log(currentClock)
-
-      const cartographic = Cartographic.fromCartesian(activeEntityPosition)
-
-      const longRadians = cartographic.longitude
-      const latRadians = cartographic.latitude
-
-      const latDegrees = Math.toDegrees(latRadians)
-      const longDegrees = Math.toDegrees(longRadians)
-
-      return { latitude: latDegrees, longitude: longDegrees }
-    }
-    return { latitude: 0, longitude: 0 }
-  }
-
-  const getWeather = async (): Promise<void> => {
-    const { latitude, longitude } = getCoordinates()
-
-    console.log(data)
-
-    // try {
-    //   const response = await fetch(``)
-
-    //   if (!response.ok) {
-    //     throw new Error(`HTTP error! Status: ${response.status}`);
-    //   }
-
-    //   const data = await response.json();
-    //   console.log("🚀 ~ file: WeatherForm.tsx:65 ~ getWeather ~ data", data)
-    //   return data;
-    // } catch (error) {
-    //   console.error('Error:', error);
-    //   throw error;
-    // }
-  }
-
+export function WeatherForm({ weatherData, currentClock }: WeatherFormProps): JSX.Element {
   return (
     <div className="weather-form">
-      <NowCard weatherData={data} currentClock={currentClock} />
-      <DayCard weatherData={data} currentClock={currentClock} mid={'am'} />
-      <DayCard weatherData={data} currentClock={currentClock} mid={'pm'} />
-      <JsonCard weatherData={data} />
-      <button onClick={(): Promise<void> => getWeather()}>Hello</button>
+      {weatherData && <NowCard weatherData={weatherData} currentClock={currentClock} />}
+      {weatherData && <DayCard weatherData={weatherData} currentClock={currentClock} mid={'am'} />}
+      {weatherData && <DayCard weatherData={weatherData} currentClock={currentClock} mid={'pm'} />}
+      {weatherData && <JsonCard weatherData={weatherData} />}
     </div>
   )
 }
