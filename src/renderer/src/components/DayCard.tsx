4f72c974467471c0567021f9c75d88f50d4cf084
Duckiduc <git@ducki.dev>
2023-10-18 17:17:11 +0200

    Refactor DayCard

--- src/renderer/src/components/DayCard.tsx
+++ src/renderer/src/components/DayCard.tsx
@@ -1,16 +1,16 @@
 import { GregorianDate } from 'cesium'
-import './DayCard.css'
 import { WeatherData } from '../types/weatherData'
+import './DayCard.css'
 
 interface DayCardProps {
-  weatherData: WeatherData
   currentClock: GregorianDate | null
   mid: string
+  weatherData: WeatherData
 }
 
-export function DayCard({ weatherData, currentClock, mid }: DayCardProps): JSX.Element {
-  const hours =
-    mid === 'am' ? weatherData.days[0].hours.slice(0, 12) : weatherData.days[0].hours.slice(12)
+export function DayCard({ currentClock, mid, weatherData }: DayCardProps): JSX.Element {
+  const isAM = mid === 'am'
+  const hours = isAM ? weatherData.days[0].hours.slice(0, 12) : weatherData.days[0].hours.slice(12)
 
   const shift = weatherData.tzoffset > 0 ? `+${weatherData.tzoffset}` : weatherData.tzoffset
   const currentClockTz = (currentClock?.hour ?? 0) + weatherData.tzoffset ?? 0
@@ -18,20 +18,17 @@ export function DayCard({ weatherData, currentClock, mid }: DayCardProps): JSX.E
   return (
     <div className="card">
       <div className="tz">UTC{shift}</div>
-      {hours.map((hour, index) => (
-        <div
-          key={index}
-          className={`hour-cell ${
-            currentClockTz >= 0 && (mid === 'am' ? index : index + 12) === currentClockTz
-              ? 'black'
-              : ''
-          }`}
-        >
-          <div>{mid === 'am' ? index : index + 12}</div>
-          <br />
-          <div>{hour.temp}</div>
-        </div>
-      ))}
+      {hours.map((hour, index) => {
+        const hourIndex = isAM ? index : index + 12
+        const isCurrentHour = currentClockTz >= 0 && hourIndex === currentClockTz
+        return (
+          <div key={index} className={`hour-cell ${isCurrentHour ? 'black' : ''}`}>
+            <div>{hourIndex}</div>
+            <br />
+            <div>{hour.temp}</div>
+          </div>
+        )
+      })}
     </div>
   )
 }
