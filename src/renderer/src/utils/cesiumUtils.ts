f3b9f5dc4229b52a8785d379201f500abfc1e644
Duckiduc <git@ducki.dev>
2023-10-18 17:16:42 +0200

    Refactor cesiumUtils and use metric unit system

--- src/renderer/src/utils/cesiumUtils.ts
+++ src/renderer/src/utils/cesiumUtils.ts
@@ -17,30 +17,25 @@ export const getCoordinates = (
   viewerRef: RefObject<Viewer | null>,
   handleClockUpdate: (date: GregorianDate) => void
 ): { latitude: number; longitude: number; date: GregorianDate } => {
-  if (viewerRef.current) {
-    const mainDataSource = viewerRef.current.dataSources.get(0)
-    const entitiesUnkown = mainDataSource.entities as unknown
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const activeEntity = (entitiesUnkown as { _entities: any })._entities?._array[0]
-    const activeEntityPosition = activeEntity._position.getValue(
-      viewerRef.current.clock.currentTime
-    )
-
-    const date = JulianDate.toGregorianDate(viewerRef.current.clock.currentTime)
-
-    handleClockUpdate(date)
-
-    const cartographic = Cartographic.fromCartesian(activeEntityPosition)
-
-    const longRadians = cartographic.longitude
-    const latRadians = cartographic.latitude
-
-    const latDegrees = Math.toDegrees(latRadians)
-    const longDegrees = Math.toDegrees(longRadians)
-
-    return { latitude: latDegrees, longitude: longDegrees, date }
+  if (!viewerRef.current) {
+    return { latitude: 0, longitude: 0, date: defaultDate }
   }
-  return { latitude: 0, longitude: 0, date: defaultDate }
+
+  const mainDataSource = viewerRef.current.dataSources.get(0)
+  const entitiesUnkown = mainDataSource.entities as unknown
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const activeEntity = (entitiesUnkown as { _entities: any })._entities?._array[0]
+  const activeEntityPosition = activeEntity._position.getValue(viewerRef.current.clock.currentTime)
+
+  const date = JulianDate.toGregorianDate(viewerRef.current.clock.currentTime)
+  handleClockUpdate(date)
+
+  const cartographic = Cartographic.fromCartesian(activeEntityPosition)
+
+  const latDegrees = Math.toDegrees(cartographic.latitude)
+  const longDegrees = Math.toDegrees(cartographic.longitude)
+
+  return { latitude: latDegrees, longitude: longDegrees, date }
 }
 
 export const fetchWeatherData = async (
@@ -51,8 +46,13 @@ export const fetchWeatherData = async (
 ): Promise<WeatherData> => {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latitude}%2C${longitude}/${date.year}-${date.month}-${date.day}/${date.year}-${date.month}-${date.day}?unitGroup=us&include=days%2Chours&key=${weatherApiKey}&contentType=json`
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latitude}%2C${longitude}/${date.year}-${date.month}-${date.day}/${date.year}-${date.month}-${date.day}?unitGroup=metric&include=days%2Chours&key=${weatherApiKey}&contentType=json`
     )
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch weather data')
+    }
+
     const data = await response.json()
     return data
   } catch (error) {
